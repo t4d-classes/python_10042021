a1699c1b617e1915d6e5a040649538ea47fe0814
Eric Greene <eric@t4d.io>
2021-10-04 15:41:48 -0400

    add code demos

--- user_tools/src/user_tools/__main__.py
+++ user_tools/src/user_tools/__main__.py
@@ -1,8 +1,33 @@
 """ user tools app """
 
+import random
 
-print(__name__)
 
 if __name__ == "__main__":
 
-    print("Hello, World")
+    password_length = int(input("Please enter a password length: "))
+    print("Password length: " + str(password_length))
+
+    char_pool = []
+
+    char_pool.append('A')
+    char_pool.append('B')
+    char_pool.append('C')
+    char_pool.append('1')
+    char_pool.append('2')
+    char_pool.append('3')
+
+    # print(len(char_pool))
+    # print(char_pool)
+    # print(",".join(char_pool))
+
+    generated_password = []
+
+    for _ in range(password_length):
+        char_pool_index = random.randrange(0, len(char_pool))
+        generated_password.append(char_pool[char_pool_index])
+
+    print("".join(generated_password))
+
+
+
