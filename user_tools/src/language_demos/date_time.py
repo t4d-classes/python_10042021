b9b82e03dfaa0c8709c57e02b7fb721a47e0cbbe
Eric Greene <eric@t4d.io>
2021-10-05 14:02:11 -0400

    add exercise 1

---
+++ user_tools/src/language_demos/date_time.py
@@ -0,0 +1,54 @@
+""" date time module """
+
+from datetime import date, datetime, timedelta
+import time
+
+
+# start = datetime.now()
+
+# end = start + timedelta(days=180)
+
+# print(start)
+# print(end)
+
+# print(type(end - start))
+# print(end - start)
+
+# print("a")
+
+# time.sleep(2)
+
+# print("b")
+
+# independence_day = date(1776, 7, 4)
+
+# print(independence_day)
+# print(type(independence_day))
+# print(type(datetime.now()))
+
+# print(datetime.now().strftime("%B %A"))
+
+# tax_day_str = "05-2021-17"
+
+# tax_day = datetime.strptime(tax_day_str, "%m-%Y-%d")
+
+# print(tax_day)
+
+# if datetime.now() > tax_day:
+#     print("tax day has passed, hope you paid your taxes...")
+# else:
+#     print("tax day is coming up, better get busy preparing your forms...")
+
+
+# start = time.time()
+
+# print(start)
+
+# time.sleep(2)
+
+# end = time.time()
+
+# print(end)
+
+# print(end - start)
+# print(type(end - start))
