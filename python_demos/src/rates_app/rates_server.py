330c0540586a35452094ed83f9a8217a3e3ee0af
Eric Greene <eric@t4d.io>
2021-10-07 19:16:45 -0400

    add client count solution

--- python_demos/src/rates_app/rates_server.py
+++ python_demos/src/rates_app/rates_server.py
@@ -1,9 +1,11 @@
 """ rate server module """
 
 from typing import Optional
+from multiprocessing.sharedctypes import Synchronized
 import multiprocessing as mp
 import sys
 import socket
+import threading
 
 # Create "ClientConnectionThread" class that inherits from "Thread"
 
@@ -11,7 +13,33 @@ import socket
 # "ClientConnectionThread" class. The class is responsible for sending the
 # welcome message and interacting with the client, echoing messages
 
-def rate_server(host: str, port: int) -> None:
+
+class ClientConnectionThread(threading.Thread):
+    """ client connection thread """
+
+    def __init__(
+        self,
+        conn: socket.socket,
+        client_count: Synchronized) -> None:
+        threading.Thread.__init__(self)
+        self.conn = conn
+        self.client_count = client_count
+
+    def run(self) -> None:
+
+        self.conn.sendall(b"Connected to the Rate Server")
+
+        while True:
+            message = self.conn.recv(2048)
+            if not message:
+                break
+            self.conn.sendall(message)
+
+        with self.client_count.get_lock():
+            self.client_count.value -= 1             
+
+
+def rate_server(host: str, port: int, client_count: Synchronized) -> None:
     """rate server"""
 
     with socket.socket(
@@ -20,27 +48,33 @@ def rate_server(host: str, port: int) -> None:
         socket_server.bind( (host, port) )
         socket_server.listen()
 
-        conn, _ = socket_server.accept()
-
-        conn.sendall(b"Connected to the Rate Server")
-
         while True:
-            message = conn.recv(2048)
-            conn.sendall(message)    
+
+            conn, _ = socket_server.accept()
+
+            with client_count.get_lock():
+                client_count.value += 1
+
+            client_con_thread = ClientConnectionThread(conn, client_count)
+            client_con_thread.start()
+
+          
 
 
 class RateServerError(Exception):
     """ rate server error class """
 
 
-def command_start_server(server_process: Optional[mp.Process]) -> mp.Process:
+def command_start_server(
+    server_process: Optional[mp.Process],
+    client_count: Synchronized) -> mp.Process:
     """ command start server """
 
     if server_process and server_process.is_alive():
         print("server is already running")
     else:
         server_process = mp.Process(
-            target=rate_server, args=('127.0.0.1', 5000))
+            target=rate_server, args=('127.0.0.1', 5000, client_count))
         server_process.start()
         print("server started")
 
@@ -70,6 +104,10 @@ def command_server_status(server_process: Optional[mp.Process]) -> None:
     else:
         print("server is stopped")
 
+def command_client_count(client_count: int) -> None:
+    """ command client count """
+    print(f"{client_count} client(s) connected")
+
 def command_exit(server_process: Optional[mp.Process]) -> None:
     """ exit the rates server app """
 
@@ -82,6 +120,7 @@ def main() -> None:
 
     try:
 
+        client_count: Synchronized = mp.Value('i', 0)
         server_process: Optional[mp.Process] = None
 
         while True:
@@ -89,11 +128,13 @@ def main() -> None:
             command = input("> ")
 
             if command == "start":
-                server_process = command_start_server(server_process)
+                server_process = command_start_server(server_process, client_count)
             elif command == "stop":
                 server_process = command_stop_server(server_process)
             elif command == "status":
                 command_server_status(server_process)
+            elif command == "count":
+                command_client_count(client_count.value)
             elif command == "exit":
                 command_exit(server_process)
                 break
